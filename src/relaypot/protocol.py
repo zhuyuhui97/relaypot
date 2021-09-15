04fc8acc77d77822bfd02db45cb80bd9346cf30c
ZHU Yuhui <zhuyuhui@163.com>
2021-09-15 19:53:12 +0800

    implement Upstream Service and related classes

--- src/relaypot/protocol.py
+++ src/relaypot/protocol.py
@@ -2,6 +2,7 @@ from twisted.internet.protocol import Protocol
 from twisted.python import log, failure, components
 from twisted.logger import Logger
 from twisted.internet import reactor
+from twisted.application.internet import TCPClient
 
 import relaypot.factory
 from relaypot.util import create_endpoint_services
@@ -14,20 +15,39 @@ class MyProtocol(Protocol):
     log = Logger()
 
     def connectionMade(self):
-        self.host_addr = self.transport.getHost()
+        self.host_addr = self.transprt.getHost()
         self.peer_addr = self.transport.getPeer()
         self.log.info("Got conn {addr} -> :{port}",
                       addr=self.peer_addr.host, port=self.host_addr.port)
+        self.setup_upstream()
+        # set session info here
+        #self.make_upstream_conn()
 
     def connectionLost(self, reason: failure.Failure):
         self.log.info("Lost conn {addr} -> :{port}",
                       addr=self.peer_addr.host, port=self.host_addr.port)
+        self.close_upstream()
 
     def dataReceived(self, data: bytes):
         self.log.info("Got data {addr} -> :{port} = {data!r}",
                       addr=self.peer_addr.host, port=self.host_addr.port, data=data)
         # reactor
         self.start_service()
+
+    def setup_upstream(self):
+        self.upstream = TCPClient()
+        
+    def set_upstream(self, downstream):
+        self.downstream = downstream()
+
+    def close_upstream(self):
+        pass
+
+    def send_upstream(self, buf):
+        pass
+
+    def recv_upstream(self, buf):
+        self.transport.write(buf)
 
     def start_service(self):
         factory = relaypot.factory.MyFactory()
