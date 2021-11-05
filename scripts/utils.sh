ec5805772dc20287efd9deb5890a0b29a20e7bbd
ZHU Yuhui <zhuyuhui@163.com>
2021-11-05 15:30:06 +0800

    Update utils.sh to allow multiple backend instances

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -4,8 +4,7 @@ BKMOD='relaypot-backend'
 RUN_DIR='/var/run/relaypot'
 LOG_DIR='/var/log/relaypot'
 
-
-init_dirs(){
+init_dirs() {
     if [ ! -d $RUN_DIR ]; then
         sudo mkdir -p $RUN_DIR
         sudo chown pot:pot $RUN_DIR
@@ -17,24 +16,23 @@ init_dirs(){
     fi
 }
 
-start(){
+start() {
     if [ ! -z $2 ]; then
-            CFG_PARAM="-c $2"
+        CFG_PARAM="-c $2"
     fi
-    if [ $1 = 'backend' ]; then
-        
-        PYTHONPATH=$RELAYPOT_HOME/src twistd --pidfile=$RUN_DIR/backend.pid --logfile=$LOG_DIR/backend.log $BKMOD $CFG_PARAM
-    else 
+    echo $1 | grep 'backend' 
+    if [ $? -eq 0 ]; then # If $1 contains 'backend', run backend but give a special name for running files.
+        PYTHONPATH=$RELAYPOT_HOME/src twistd --pidfile=$RUN_DIR/$1.pid --logfile=$LOG_DIR/$1.log $BKMOD $CFG_PARAM
+    else
         PYTHONPATH=$RELAYPOT_HOME/src twistd --pidfile=$RUN_DIR/$1.pid --logfile=$LOG_DIR/$1.log $FRMOD -p $1 $CFG_PARAM
     fi
 }
 
-
-stop(){
-    kill `cat $RUN_DIR/$1.pid`
+stop() {
+    kill $(cat $RUN_DIR/$1.pid)
 }
 
-watch(){
+watch() {
     tail -f $LOG_DIR/$1.log
 }
 
