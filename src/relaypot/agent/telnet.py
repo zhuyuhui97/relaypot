d41eeb62a0a3b950fcb40454088d8f47f1655b4c
ZHU Yuhui <zhuyuhui@163.com>
2021-11-17 19:24:08 +0800

    New telnet request handler, profile won't work now.

--- src/relaypot/agent/telnet.py
+++ src/relaypot/agent/telnet.py
@@ -15,10 +15,10 @@ class Agent(BaseAgent):
     STATUS_REQ_PASSWORD = 2
     STATUS_AUTH_DONE = 3
     NON_PRINTABLE = itertools.chain(range(0x00, 0x20), range(0x7f, 0xa0))
-    blacklist_name='blacklist.txt'
-    blacklist_base='blacklist'
+    blacklist_name = 'blacklist.txt'
+    blacklist_base = 'blacklist'
 
-    def __init__(self, fproto:protocol.Protocol, profile_name=None, profile_base='profiles'):
+    def __init__(self, fproto: protocol.Protocol, profile_name=None, profile_base='profiles'):
         plist = os.listdir(profile_base)
         if profile_name == None:
             rnd = random.randrange(0, len(plist))
@@ -30,9 +30,9 @@ class Agent(BaseAgent):
         self.load_blacklist()
         self.status = self.STATUS_NO_AUTH
         self.line_buffer = b''
-    
+
     def load_blacklist(self):
-        self.blacklist=[]
+        self.blacklist = []
         filepath = os.path.join(self.blacklist_base, self.blacklist_name)
         with open(filepath) as pfile:
             while True:
@@ -60,52 +60,33 @@ class Agent(BaseAgent):
         self.status = self.STATUS_REQ_USERNAME
         return [b'\xff\xfd\x01\xff\xfd\x1f\xff\xfb\x01\xff\xfb\x03R6300V2-14EF login: ']
 
-    # def on_request(self, buf:bytes):
-    #     self.line_buffer = self.line_buffer.join(buf)
-    #     if self.status == self.STATUS_NO_AUTH:
-    #         pass # TODO Username sent without any probe
-    #     if self.status == self.STATUS_REQ_USERNAME:
-    #         self.status = self.STATUS_REQ_PASSWORD
-    #         self.username = buf.strip()
-    #         return [self.username, '\r\nPassword: ']
-    #     elif self.status == self.STATUS_REQ_PASSWORD:
-    #         self.status = self.STATUS_AUTH_DONE
-    #         return [b'\r\n\r\n\r\nASUSWRT-Merlin R6300V2 380.70-0-X7.9.1 Tue Sep 25 11:47:13 UTC 2018\r\n', self.ps1]
-    #     else:
-    #         resp = [buf] # echo
-    #         if self.line_buffer.endswith(b'\r\n'):
-    #             resp.extend(self.get_resp(self.line_buffer))
-    #             self.line_buffer = b''
-    #             resp.append(self.ps)
-    #         return resp
-
-    def on_request(self, buf:bytes):
-        self.line_buffer = self.line_buffer.join(buf)
+    def on_request(self, buf: bytes):
+        END_WITH_NL = buf.endswith(b'\r\n') or buf.endswith(b'\r\x00')
+        self.line_buffer = self.line_buffer + buf.strip()
         if self.status == self.STATUS_REQ_PASSWORD:
-            resp = [] # No echo when input password
+            resp = []  # No echo when input password
         else:
-            resp = [buf] # Echo
-        
-        if self.line_buffer.endswith(b'\r\n'):
+            resp = [buf]  # Echo
+
+        if END_WITH_NL:
             if self.status == self.STATUS_NO_AUTH:
-                pass # TODO Username sent without any probe
+                pass  # TODO Username sent without any probe
             elif self.status == self.STATUS_REQ_USERNAME:
                 self.status = self.STATUS_REQ_PASSWORD
                 self.username = buf.strip()
                 resp.append('\r\nPassword: ')
             elif self.status == self.STATUS_REQ_PASSWORD:
                 self.status = self.STATUS_AUTH_DONE
-                resp.append(b'\r\n\r\n\r\nASUSWRT-Merlin R6300V2 380.70-0-X7.9.1 Tue Sep 25 11:47:13 UTC 2018\r\n')
+                resp.append(
+                    b'\r\n\r\n\r\nASUSWRT-Merlin R6300V2 380.70-0-X7.9.1 Tue Sep 25 11:47:13 UTC 2018\r\n')
                 resp.append(self.ps1)
             else:
-                if self.line_buffer.endswith(b'\r\n'):
-                    resp.extend(self.get_resp(self.line_buffer))
-                    self.line_buffer = b''
-                    resp.append(self.ps)
+                resp.append(b'\r\n')
+                resp.extend(self.get_resp(self.line_buffer))
+                self.line_buffer = b''
+                resp.append(b'\r\n')
+                resp.append(self.ps)
         return resp
-        
-        
-        
 
     def on_front_lost(self, reason: failure.Failure):
         return None
@@ -121,12 +102,12 @@ class Agent(BaseAgent):
             black = False
             for black_item in self.blacklist:
                 if black_item in cmd:
-                    black=True
+                    black = True
                     break
             if black:
                 responses.append(b'sh: command not found.')
             elif b'echo' in cmd:
-                subp = subprocess.run(cmd,stdout=subprocess.PIPE, shell=True)
+                subp = subprocess.run(cmd, stdout=subprocess.PIPE, shell=True)
                 subp.check_returncode()
                 responses.append(subp.stdout)
             else:
